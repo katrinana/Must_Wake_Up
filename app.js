0e091ed0dcc222466ec09829e01963db735dcc2a
kevinzhuwenyu <kevinzhuwenyu@gmail.com>
2017-02-24 12:53:05 -0500

    time picker

--- app.js
+++ app.js
@@ -16,3 +16,19 @@ function alarmstate(name){
 	console.log('switch button clicked', name);
 }
 
+function gettime() {
+  var h = document.getElementById('choose-hour').value;
+  var m = document.getElementById('choose-min').value;
+  var zone = document.getElementById('choose-time').value;
+
+  console.log('hour is', h);
+  console.log('min is', m);
+  console.log("AM PM is ", zone);
+}
+function addOption(event) {
+  const option = document.createElement('option');
+  let text = document.getElementById('optionLabel').value;
+  option.innerText = text;
+  text = '';
+  document.getElementById('dynamic-sel').appendChild(option);
+}
